405c84c2e80dcf5f462f16cc4c8482a819d9c4eb
kunal1007 <kunal.jflash.1094@gmail.com>
2021-03-20 13:33:02 -0600

    Update Create_Tables.sql
    Changed the SSN Constraint from 11 to 9

--- Create_Tables.sql
+++ Create_Tables.sql
@@ -18,7 +18,7 @@ DROP TABLE IF EXISTS Customer;
 
 CREATE TABLE Customer (Customer_ID INTEGER NOT NULL,
 Customer_name VARCHAR (50) NOT NULL,
-SSN CHAR(11) NOT NULL,
+SSN CHAR(9) NOT NULL,
 Phone_number VARCHAR(12),
 Email_address VARCHAR(200),
 Address_line_1 VARCHAR (200),
