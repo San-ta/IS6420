506d098d9b489bc1863f603ed3a3e5ca9d0a931d
kunal1007 <kunal.jflash.1094@gmail.com>
2021-03-20 23:40:25 -0600

    Update Test_Queries.sql
    Added few queries that would help us understand most active customers, securities in demand.

--- Test_Queries.sql
+++ Test_Queries.sql
@@ -11,3 +11,21 @@ SELECT * FROM crypto c2;
 SELECT * FROM moving_price mp; 
 SELECT * FROM exchange e3; 
 SELECT * FROM listed_on lo;
+
+-- Query to know which customer placed the highest number of orders
+SELECT customer_name, COUNT(DISTINCT (oh.order_id)) AS total_orders_for_securities, SUM(quantity) AS total_quantity_of_securities
+FROM order_header oh
+	INNER JOIN customer c 
+		ON c.customer_id = oh.customer_id
+	INNER JOIN security_header sh 
+		ON sh.order_id = oh.order_id 
+GROUP BY customer_name
+ORDER BY total_orders_for_securities DESC; 
+
+-- Query to understand securities in demand
+SELECT sh.ticker_id, sh.security_name, SUM(quantity) AS total_quantity 
+FROM security_header sh
+	INNER JOIN order_header oh 
+		ON oh.order_id = sh.order_id 
+GROUP BY sh.ticker_id, sh.security_name 
+ORDER BY total_quantity DESC; 
